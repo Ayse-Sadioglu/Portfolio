548582a218317b1f5caa394305f28e3532129564
Ayse <aysesadioglu@protonmail.com>
2022-12-15 14:51:20 +0300

    recent updates

--- src/App.js
+++ src/App.js
@@ -2,29 +2,43 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import "./App.css";
 import "./styles/Global.css";
-import Particles from "./components/Particles"
-import Intro from "./components/Intro"
-import About from "./components/About"
-import Contact from "./components/Contact"
-
+import Particles from "./components/Particles";
+import Intro from "./components/Intro";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div>
-      <Particles/>
-      <Navbar/>
-      <Intro/>
-      <About/>
-      <Contact/>
-      
-      
-      
-      
-      
-
-      
-     
-     
+      <Particles />
+      <Navbar />
+      <div class="grid-container">
+        <div class="grid-item">
+          <Intro/>
+        </div>
+        <div class="grid-item">
+          <img src={require("./assets/obiwan.gif")} alt="obiwan" />
+        </div>
+        <div class="grid-item">
+          <img src={require("./assets/computercat2.gif")} alt="obiwan" />
+        </div>
+        <div class="grid-item">
+          <About/>
+        </div>
+        <div class="grid-item">
+        
+        </div>
+        <div class="grid-item">
+          
+        </div>
+        <div class="grid-item2">
+        <Contact/>
+         
+        </div>
+        <div class="grid-item2">
+        
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,25 +3,19 @@ import "../styles/Intro.css";
 
 class Intro extends React.Component {
   render() {
+    
     return (
-      <div id="intro">
-
-       {/* <div class="obiwan">
-          <img src={require("../assets/obiwan.gif")} alt="obiwan" /> 
-        </div>*/}
-
-        <div className="intro-title">Hello there</div>
+      <div class="Intro">
+         <div className="intro-title">Hello there</div>
           <div className="intro-description">
             I'm Ayse and this is my website built with React.js 
+            
+            
           </div>
-          
-          <div class="obiwan">
-          <img src={require("../assets/obiwan.gif")} alt="obiwan" /> 
-        </div>
-
-        
+      
       </div>
     );
+    
   }
 }
 
