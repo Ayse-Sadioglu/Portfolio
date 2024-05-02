275365a4b5aca253d2445d7a24ba68709349fb41
Ayse <aysesadioglu@protonmail.com>
2024-05-02 18:59:56 +0300

    new version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import "./App.css";
-import "./styles/Global.css";
-import Particles from "./components/Particles";
-import Intro from "./components/Intro";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Header from "./components/Header";
+import React, { useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadFull } from "tsparticles";
+import particlesOptions from "./particles.json";
 
 function App() {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    if (init) {
+      return;
+    }
+
+    initParticlesEngine(async (engine) => {
+      await loadFull(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+
   return (
-    <div>
-     
-      <Particles />
-      <Navbar />
-      <Intro />
-     
-      <About />
-      <Contact />
+    <div className="App">
+      {init && <Particles options={particlesOptions} />}
+      <header className="App-header"></header>
     </div>
   );
 }
