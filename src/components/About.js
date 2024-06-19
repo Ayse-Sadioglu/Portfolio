e3d5809aee7ba9bfa09d94271d68f3c4d5047eff
Ayse <aysesadioglu@protonmail.com>
2024-06-19 11:49:10 +0300

    recent updates

--- src/components/About.js
+++ src/components/About.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React from "react";
 import "../styles/About.css";
-import charonImage from '../assets/charon.jpg'; 
+import charonImage from "../assets/charon.jpg";
 
+//¯\_(ツ)_/¯
 const About = () => {
   return (
     <div className="about-container">
       <div className="about-content">
+       
+        <div className="about-text">
+          <h2 className="about-header">about me</h2>
+          <p className="custom-font">Hello there, I'm Ayse.</p>
+          <p className="custom-font">
+         I'm in my final year studying Computer
+        Engineering at TOBB University of Economics and Technology. I love
+        working on my own projects and contributing to open-source
+        communities to keep learning. I'm trying to explore
+        different fields within computer engineering by getting hands-on
+        with various projects.
+      </p>
+      <p className="custom-font">
+        Outside of my studies, I'm interested in music and play the violin and piano.
+        I'm also an amateur figure skater.
+      </p>
+        </div>
         <div className="about-image">
           <img src={charonImage} alt="About" />
-        </div>
-        <div className="about-text">
-          <h2 className="about-header">¯\_(ツ)_/¯ about me</h2>
-          <p>
-             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, pulvinar facilisis justo mollis, auctor consequat urna. Morbi a bibendum metus. Donec scelerisque sollicitudin enim eu venenatis. Duis tincidunt laoreet ex, in pretium orci vestibulum eget.
-          </p>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default About;
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import "../styles/Intro.css";
 import AsciiArtPrinter from './AsciiArtPrinter ';
-
+import dovahzul from '../assets/dovahzul.png'
+import skatercat from '../assets/skatecat.gif'
 const Intro = ({ onLoaded }) => {
   const myAsciiArt = ` `;
   const letters = myAsciiArt.split('');
@@ -15,9 +16,22 @@ return (
   <div className="intro-container">
     <div className="intro-content">
       <div className="intro-text">
-        <h1 className="intro-header">Drem Yol Lok, Greetings</h1> 
-        <p className="intro-subtext">*Drem Yol Lok, which translates to 'Peace, Fire, Sky' in  <a href="https://www.thuum.org/learn/practice/phrases.php" className="intro-link">Dovahzul,</a> is used to mean 'greetings'</p>
+      <img src={skatercat} alt="Intro GIF" className="intro-image" />
 
+        <h1 className="intro-header">Drem Yol Lok, Greetings</h1>
+
+        {/* <img src={dovahzul} alt="Intro Image" className="intro-image" /> */}
+
+        <p className="intro-subtext">
+          *Drem Yol Lok, which translates to 'Peace, Fire, Sky'  in{" "}
+          <a
+            href="https://www.thuum.org/learn/practice/phrases.php"
+            className="intro-link"
+          >
+            Dovahzul,
+          </a>{" "}
+          is used to mean 'greetings'
+        </p>
       </div>
     </div>
   </div>
